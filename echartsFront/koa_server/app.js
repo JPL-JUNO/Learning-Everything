b775df7edf24febb9d70a92c1218567a3d4a9b56
Stephen CUI <junocuixuan@163.com>
2023-01-29 23:01:27 +0800

    20130129

--- echartsFront/koa_server/app.js
+++ echartsFront/koa_server/app.js
@@ -3,7 +3,12 @@ const Koa = require("koa");
 const app = new Koa();
 
 const responseDurationMiddleWave = require("./middleWave/koa_response_duration");
-
 app.use(responseDurationMiddleWave);
+
+const responseHeaderMiddleWave = require("./middleWave/koa_response_header");
+app.use(responseHeaderMiddleWave);
+
+const responseDataMiddleWave = require("./middleWave/koa_response_data");
+app.use(responseDataMiddleWave);
 
 app.listen("8888");
