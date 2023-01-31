46689ec4bac26263c6f3af1939622cad7cfb1ea6
Stephen CUI <junocuixuan@163.com>
2023-01-31 23:10:31 +0800

    20230131

--- echartsFront/visualization/src/main.js
+++ echartsFront/visualization/src/main.js
@@ -1,12 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
 
-Vue.config.productionTip = false
+import "./assets/css/global.less";
+
+// 请求基准路径的配置
+axios.defaults.baseURL = "http://127.0.0.1:8888/api";
+// 将axios挂载到Vue的原型对象上，这里的http是自己起的名称
+// 在别的组件中使用this.$http 就可以得到对象，从而发起ajax请求
+Vue.prototype.$http = axios;
+
+// 将全局的echarts对象挂载到Vue的原型对象上
+// 别的组件中 this.$echarts 使用
+Vue.prototype.$echarts = window.echarts;
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ echartsFront/visualization/src/views/sellerPage.vue
@@ -0,0 +1,22 @@
+<!-- 针对/sellerPage这条路径而显示出来的 -->
+<!-- 在这个组件中，通过显示子组件注册的方式，显示出seller.vue这个组件 -->
+<template>
+    <div class="com-page">
+        <seller></seller>
+    </div>
+</template>
+
+<script>
+import Seller from '@/components/seller.vue'
+export default {
+    data() { 
+        return {}
+    },
+    methods: {},
+    components: {
+        seller: Seller
+    }
+}
+</script>
+<style>
+</style>
